4825ed8e6015585b6b2b822f4f0f822c59629d07
Rafiere <rafapgbr@gmail.com>
2023-12-24 17:26:39 -0300

    feat: add base DDD types

---
+++ src/modules/@shared/domain/entity/aggregate-root.interface.ts
@@ -0,0 +1,4 @@
+/* Essa interface servirá para demarcar as entidades que são um
+* "aggregate root". */
+
+export default interface AggregateRoot {}
---
+++ src/modules/@shared/domain/entity/value-object/value-object.interface.ts
@@ -0,0 +1,5 @@
+/* Essa será uma interface de marcação para definirmos value objects. */
+
+export default interface ValueObject {
+
+}
