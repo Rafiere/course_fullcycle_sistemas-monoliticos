2c7848706660065d812e311c71379717cfb2ac30
Rafiere <rafapgbr@gmail.com>
2023-12-25 17:01:00 -0300

    feat: add repository implementation and test for find all products usecase

---
+++ src/modules/store-catalog/repository/product.repository.spec.ts
@@ -0,0 +1,59 @@
+import {Sequelize} from "sequelize-typescript";
+import ProductModel from "./product.model";
+import ProductRepository from "./product.repository";
+
+describe("Product repository test", () => {
+
+    let sequelize: Sequelize
+
+    /* Antes de cada teste, será aberta uma conexão com o banco de dados. */
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: 'sqlite',
+            storage: ':memory:',
+            logging: false,
+            sync: {force: true}
+        })
+
+        sequelize.addModels([ProductModel])
+        await sequelize.sync()
+    })
+
+    /* Após cada teste, a conexão será encerrada. */
+    afterEach(async () => {
+        await sequelize.close();
+    })
+
+    it("should find all products", async () => {
+
+        const product01 = await ProductModel.create({
+            id: "1",
+            name: "Product 01",
+            description: "Product 01 description",
+            salesPrice: 100,
+        })
+
+        const product02 = await ProductModel.create({
+            id: "2",
+            name: "Product 02",
+            description: "Product 02 description",
+            salesPrice: 200,
+        })
+
+        const productRepository = new ProductRepository();
+
+        const products = await productRepository.findAll();
+
+        expect(products.length).toBe(2);
+
+        expect(products[0].id.id).toBe(product01.id);
+        expect(products[0].name).toBe(product01.name);
+        expect(products[0].description).toBe(product01.description);
+        expect(products[0].salesPrice).toBe(product01.salesPrice);
+
+        expect(products[1].id.id).toBe(product02.id);
+        expect(products[1].name).toBe(product02.name);
+        expect(products[1].description).toBe(product02.description);
+        expect(products[1].salesPrice).toBe(product02.salesPrice);
+    })
+})
---
+++ src/modules/store-catalog/repository/product.repository.ts
@@ -0,0 +1,23 @@
+import ProductGateway from "../gateway/product.gateway";
+import Product from "../domain/product.entity";
+import ProductModel from "./product.model";
+import Id from "../../@shared/domain/entity/value-object/id.value-object";
+
+export default class ProductRepository implements ProductGateway {
+
+    async findAll(): Promise<Product[]> {
+
+        const products = await ProductModel.findAll();
+
+        return products.map((product) => new Product({
+            id: new Id(product.id),
+            name: product.name,
+            description: product.description,
+            salesPrice: product.salesPrice,
+        }));
+    }
+
+    find(id: string): Promise<Product> {
+        return Promise.resolve(undefined);
+    }
+}
