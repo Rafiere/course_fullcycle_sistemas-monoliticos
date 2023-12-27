6cc3deb969c205e20a47767e75c901ce571993ec
Rafiere <rafapgbr@gmail.com>
2023-12-27 08:36:26 -0300

    feat: add factory for payment facade

---
+++ src/modules/payment/factory/payment.facade.factory.ts
@@ -0,0 +1,13 @@
+import PaymentFacade from "../facade/payment.facade";
+import PaymentFacadeInterface from "../facade/facade.interface";
+import TransactionRepository from "../repository/transaction.repository";
+import ProcessPaymentUsecase from "../usecase/process-payment/process-payment.usecase";
+
+export default class PaymentFacadeFactory {
+    static create(): PaymentFacadeInterface {
+
+        const transactionRepository = new TransactionRepository();
+        const processPaymentUsecase = new ProcessPaymentUsecase(transactionRepository);
+        return new PaymentFacade(processPaymentUsecase);
+    }
+}
